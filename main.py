fcd612354d763536f4852cfff76829a83b9b1dfa
labyrinthofdreams <noemail@localhost.localhost>
2018-09-02 17:06:17 +0300

    Add: phpBB support

--- main.py
+++ main.py
@@ -48,36 +48,36 @@ def find_posts(html):
     """Finds all posts in the given bs4 html object
     
     Returns empty list if finds none, otherwise a list of all found posts"""
-    table = html.find('table', {'id': 'topic_viewer'})
     posts = []
-    if not table:
-        return posts
-    rows = table.find_all('tr', id=re.compile('post-[0-9]{7}'))
+    rows = html.find_all('div', class_='post')
     for row in rows:
         post = {}
         post['id'] = row.get('id')
-        post['text'] = table.find('tr', id=row.get('id')).find_next_sibling().\
-                                                find('td', class_='c_post')
-        post['user'] = {'username': row.find('a', class_='member').string,
-                        'profile': row.find('a', class_='member').get('href')}
+        post['text'] = row.find('div', class_='content')
+        post['user'] = {'username': row.find('a', class_='username-coloured').string,
+                        'profile': row.find('a', class_='username-coloured').get('href')}
         posts.append(post)
     return posts
 
 def get_page_count(thread_id):
     """Returns the number of pages in a thread as an integer"""
-    url = os.path.join(FORUMURL, 'topic/{0}/1?x=25'.format(thread_id))
+    url = os.path.join(FORUMURL, 'viewtopic.php?t={0}'.format(thread_id))
     response = session.get(url)
     html = bs4.BeautifulSoup(response.text, 'html5lib')
-    pages = html.find('ul', class_='cat-pages')
+    pages = html.find('div', class_='pagination')
     if not pages:
         return 1
-    # Get last <li> element
-    li = pages.find_all('li')[-1]
-    return int(li.string)
+    # Get last <strong> element if only 1 page
+    nolink = pages.find_all('strong')
+    if nolink:
+        return int(nolink[-1].string)
+    # Has more than one page    
+    links = pages.find_all('li')[-2].find('a').string
+    return int(links)
     
 def get_posts(thread_id, page):
     """Returns all the posts in a given thread_id on a given page"""
-    url = os.path.join(FORUMURL, 'topic/{0}/{1}?x=25'.format(thread_id, page))
+    url = os.path.join(FORUMURL, 'viewtopic.php?t={0}&start={1}'.format(thread_id, ((page - 1) * 40)))
     response = session.get(url)
     html = bs4.BeautifulSoup(response.text, 'html5lib')
     return find_posts(html)
@@ -112,48 +112,48 @@ def get_index(iterable, fun):
     
 def attr(html, name):
     """Get input value with a matching name attribute"""
-    return html.find('input', attrs={'name': name})['value'] 
+    return (None, html.find('input', attrs={'name': name})['value'])
     
 def reached_time_limit(text):
     """Returns Rrue if posting time limit has been reached, otherwise False"""
+    # TODO
     return text.find('The frequency of posting is limited to prevent abuse') > -1
     
 def edit_post(text, forum_id, thread_id, post_id):
     """Edit forum post"""
-    url = os.path.join(FORUMURL, 'post/?mode=3&type=1&f={0}&t={1}&p={2}&pg=1'.\
-                                    format(forum_id, thread_id, post_id))
+    # Get tokens from edit post page
+    url = os.path.join(FORUMURL, 'posting.php?mode=edit&f={0}&p={1}'.format(forum_id, post_id))
     response = session.get(url)
-    if response.text.find('<td>You do not have permission to edit this post.<br />') > -1:
-        raise Exception('You do not have permission to edit this post')
+    #if response.text.find('<td>You do not have permission to edit this post.<br />') > -1:
+    #    raise Exception('You do not have permission to edit this post')
     html = bs4.BeautifulSoup(response.text, 'html5lib')
-    params = {'mode': attr(html, 'mode'),
-              'type': attr(html, 'type'),
-              'ast': attr(html, 'ast'),
-              'f': attr(html, 'f'),
-              'xc': attr(html, 'xc'),
-              't': attr(html, 't'),
-              'qhash': attr(html, 'qhash'),
-              'p': attr(html, 'p'),
-              'pg': attr(html, 'pg'),
-              'x': attr(html, 'x'),
-              'sd': attr(html, 'sd'),
-              'title': attr(html, 'title'),
-              'description': attr(html, 'description'),
-              'tags': attr(html, 'tags'),
-              'sig': attr(html, 'sig'),
-              'emo': attr(html, 'emo'),
-              'post': text}
-    headers = {u'content-type': u'application/x-www-form-urlencoded'}  
+    params = {'icon': (None, '0'),
+              'subject': attr(html, 'subject'),
+              'addbbcode20': (None, '100'),
+              'message': (None, text),
+              'lastclick': attr(html, 'lastclick'),
+              'edit_post_message_checksum': attr(html, 'edit_post_message_checksum'),
+              'edit_post_subject_checksum': attr(html, 'edit_post_subject_checksum'),
+              'post': attr(html, 'post'),
+              'attach_sig': (None, 'on'),
+              'edit_reason': (None, ''),
+              'show_panel': attr(html, 'show_panel'),
+              'creation_time': attr(html, 'creation_time'),
+              'form_token': attr(html, 'form_token')
+              }
+    # Sleeping is necessary or our post won't be saved
+    # This is apparently a bot countermeasure
+    print 'Please wait...'
+    time.sleep(10)
     try:
-        posturl = os.path.join(FORUMURL, 'post/')
-        response = session.post(posturl, data=params, headers=headers, 
-                                cookies=session.cookies)
-        if reached_time_limit(response.text):
-            raise FloodException('Reached time limit between posts')
-        elif response.status_code != requests.codes.ok:
+        posturl = os.path.join(FORUMURL, 'posting.php?mode=edit&f={0}&t={1}&p={2}'.format(forum_id, thread_id, post_id))
+        response = session.post(posturl, files=params, cookies=session.cookies)
+        #if reached_time_limit(response.text):
+        #    raise FloodException('Reached time limit between posts')
+        if response.status_code != requests.codes.ok:
             raise response.raise_for_status()
-        elif len(response.cookies) == 0:
-            pass
+        #elif len(response.cookies) == 0:
+        #    pass
             # raise Exception(u'Post failed. Not logged in!')
     except:
         raise
@@ -341,19 +341,17 @@ def check_posts(sch, delay, threads, index):
             thread['last_page'] = 1
         if 'last_post_id' not in thread:
             thread['last_post_id'] = ''
-        response = session.get(os.path.join(FORUMURL, \
-                                        'topic/{0}/1/?x=25'.format(thread_id)))
+        # Get first page of a thread
+        response = session.get(os.path.join(FORUMURL, 'viewtopic.php?t={0}'.format(thread_id)))
         html = bs4.BeautifulSoup(response.text, 'html5lib')
+        topic = html.find('h2', class_='topic-title')
         if 'forum_id' not in thread:
-            nav = html.find('ul', id='nav')
-            link = nav.find_all('li')[-3]
-            href = link.find('a')['href']
-            thread['forum_id'] = re.search('([0-9]{7})', href).group(1)
+            thread['forum_id'] = re.search('f=([0-9]+)', topic.find('a')['href']).group(1)
         if 'title' not in thread:
-            thread['title'] = html.title.string
+            thread['title'] = topic.get_text()
         if 'first_post_id' not in thread:
             thread['first_post_id'] = find_posts(html)[0]['id']
-            thread['first_post'] = thread['first_post_id'][5:] 
+            thread['first_post'] = thread['first_post_id'][1:] 
         title = '{0} - {1} of {2} - {3}'.\
             format(datetime.datetime.today().strftime(u'%H:%M:%S'),
                    index, len(threads), thread['title']) 
@@ -375,7 +373,7 @@ def check_posts(sch, delay, threads, index):
             if post['id'] == thread['first_post_id']:
                 continue
             # Ignore posts in ignore list
-            if post['id'][5:] in thread['ignore']:
+            if post['id'][1:] in thread['ignore']:
                 continue
             # Check for the overwrite command
             # It overwrites all other values in the post
@@ -402,13 +400,13 @@ def check_posts(sch, delay, threads, index):
             idx = get_index(thread['users'], lambda x: x['username'] == post['user']['username'])
             if idx > -1:
                 thread['users'][idx]['seen'] = seen_films
-                thread['users'][idx]['last_post'] = post['id'][5:]
+                thread['users'][idx]['last_post'] = post['id'][1:]
             else:
                 user = {}
                 user['username'] = post['user']['username']
                 user['profile'] = post['user']['profile'] 
                 user['seen'] = seen_films
-                user['last_post'] = post['id'][5:]
+                user['last_post'] = post['id'][1:]
                 thread['users'].append(user)
         if not has_new_updates:
             print 'No new updates'
